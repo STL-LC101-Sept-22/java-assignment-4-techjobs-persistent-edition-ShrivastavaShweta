e5fee98650849d08189b461a0071c5a9673a8c89
Shweta Shrivastava <shweta.shrivastav@gmail.com>
2023-03-25 20:44:59 -0500

    completed part 3 and 4

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,8 +1,12 @@
 package org.launchcode.techjobs.persistent.models;
 
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Employer extends AbstractEntity {
@@ -10,6 +14,10 @@ public class Employer extends AbstractEntity {
     @NotNull(message = "Location is required")
     @Size(min = 3, max=50, message = "location can be 3 to 50 characters long")
     private String location;
+
+    @OneToMany
+    @JoinColumn(name = "employer_id")
+    private final List<Job> jobs = new ArrayList<>();
 
     public Employer(String location) {
         this.location = location;
@@ -25,8 +33,14 @@ public class Employer extends AbstractEntity {
         this.location = location;
     }
 
+    public List<Job> getJobs() {
+        return jobs;
+    }
+
     @Override
     public String toString() {
         return this.getName() + location;
     }
+
+
 }
--- src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
@@ -1,13 +1,19 @@
 package org.launchcode.techjobs.persistent.models;
 
 import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
 import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Skill extends AbstractEntity {
 
     @Size(min = 3, max=300, message = "description can be 3 to 300 characters long")
     private String description;
+
+    @ManyToMany(mappedBy="skills")
+    private final List<Job> jobs = new ArrayList<>();
 
     public Skill(String description) {
         this.description = description;
@@ -22,6 +28,10 @@ public class Skill extends AbstractEntity {
 
     public void setDescription(String description) {
         this.description = description;
+    }
+
+    public List<Job> getJobs() {
+        return jobs;
     }
 
     @Override
